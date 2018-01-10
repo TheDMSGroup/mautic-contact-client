9d1530f33ac18fb7ec46bd504420a0c92ad052d7
heathdutton <hdutton@thedmsgrp.com>
2018-01-10 16:16:52 -0500

    Success Definition status codes as default.

--- Assets/js/contactclient.js
+++ Assets/js/contactclient.js
@@ -262,6 +262,7 @@ Mautic.contactclientOnLoad = function () {
             // API Payload JSON Schema.
             mQuery.getScriptCachedOnce('https://cdn.jsdelivr.net/combine/' +
                 'npm/bootstrap-slider@10,npm/bootstrap-datepicker@1,' +
+                // @todo - replace selectize with bootstrap-select where appropriate.
                 'npm/selectize@0.12.4/dist/js/standalone/selectize.min.js,' +
                 'npm/jQuery-QueryBuilder@2/dist/js/query-builder.standalone.min.js,' +
                 'gh/heathdutton/json-editor@0.7.30/dist/jsoneditor.min.js',
@@ -277,41 +278,82 @@ Mautic.contactclientOnLoad = function () {
                             var rules = {
                                     condition: 'AND',
                                     rules: [{
-                                        id: 'price',
-                                        operator: 'less',
-                                        value: 10.25
-                                    }, {
-                                        condition: 'OR',
-                                        rules: [{
-                                            id: 'category',
-                                            operator: 'equal',
-                                            value: 2
-                                        }, {
-                                            id: 'category',
-                                            operator: 'equal',
-                                            value: 1
-                                        }]
+                                        id: 'status',
+                                        operator: 'equal',
+                                        value: '200'
                                     }]
                                 },
                                 // Default filters.
                                 filters = [{
-                                    id: 'name',
-                                    label: 'Name',
-                                    type: 'string'
-                                }, {
-                                    id: 'category',
-                                    label: 'Category',
-                                    type: 'integer',
+                                    id: 'status',
+                                    label: 'Status Code',
+                                    type: 'string',
                                     input: 'select',
                                     values: {
-                                        1: 'Books',
-                                        2: 'Movies',
-                                        3: 'Music',
-                                        4: 'Tools',
-                                        5: 'Goodies',
-                                        6: 'Clothes'
+                                        '1xx': '1xx: Informational',
+                                        '100': '100: Continue',
+                                        '101': '101: Switching Protocols',
+                                        '2xx': '2xx: Successful',
+                                        '200': '200: OK',
+                                        '201': '201: Created',
+                                        '202': '202: Accepted',
+                                        '203': '203: Non-Authoritative Information',
+                                        '204': '204: No Content',
+                                        '205': '205: Reset Content',
+                                        '206': '206: Partial Content',
+                                        '3xx': '3xx: Redirection',
+                                        '300': '300: Multiple Choices',
+                                        '301': '301: Moved Permanently',
+                                        '302': '302: Found',
+                                        '303': '303: See Other',
+                                        '304': '304: Not Modified',
+                                        '305': '305: Use Proxy',
+                                        '307': '307: Temporary Redirect',
+                                        '4xx': '4xx: Client Error',
+                                        '400': '400: Bad Request',
+                                        '401': '401: Unauthorized',
+                                        '402': '402: Payment Required',
+                                        '403': '403: Forbidden',
+                                        '404': '404: Not Found',
+                                        '405': '405: Method Not Allowed',
+                                        '406': '406: Not Acceptable',
+                                        '407': '407: Proxy Authentication Required',
+                                        '408': '408: Request Timeout',
+                                        '409': '409: Conflict',
+                                        '410': '410: Gone',
+                                        '411': '411: Length Required',
+                                        '412': '412: Precondition Failed',
+                                        '413': '413: Payload Too Large',
+                                        '414': '414: URI Too Long',
+                                        '415': '415: Unsupported Media Type',
+                                        '416': '416: Range Not Satisfiable',
+                                        '417': '417: Expectation Failed',
+                                        '418': '418: I\'m a teapot',
+                                        '426': '426: Upgrade Required',
+                                        '5xx': '5xx: Server Error',
+                                        '500': '500: Internal Server Error',
+                                        '501': '501: Not Implemented',
+                                        '502': '502: Bad Gateway',
+                                        '503': '503: Service Unavailable',
+                                        '504': '504: Gateway Time-out',
+                                        '505': '505: HTTP Version Not Supported',
+                                        '102': '102: Processing',
+                                        '207': '207: Multi-Status',
+                                        '226': '226: IM Used',
+                                        '308': '308: Permanent Redirect',
+                                        '422': '422: Unprocessable Entity',
+                                        '423': '423: Locked',
+                                        '424': '424: Failed Dependency',
+                                        '428': '428: Precondition Required',
+                                        '429': '429: Too Many Requests',
+                                        '431': '431: Request Header Fields Too Large',
+                                        '451': '451: Unavailable For Legal Reasons',
+                                        '506': '506: Variant Also Negotiates',
+                                        '507': '507: Insufficient Storage',
+                                        '511': '511: Network Authentication Required',
+                                        '7xx': '7xx: Developer Error'
                                     },
-                                    operators: ['equal', 'not_equal', 'in', 'not_in', 'is_null', 'is_not_null']
+                                    operators: ['equal', 'not_equal']
                                 }, {
                                     id: 'in_stock',
                                     label: 'In stock',
@@ -358,6 +400,7 @@ Mautic.contactclientOnLoad = function () {
                                     if (rule.filter.plugin === 'selectize') {
                                         rule.$el.find('.rule-value-container').css('min-width', '200px')
                                             .find('.selectize-control').removeClass('form-control');
+
                                     }
                                 })
                                 .queryBuilder({
@@ -375,11 +418,15 @@ Mautic.contactclientOnLoad = function () {
                                 })
                                 .on('afterDeleteGroup.queryBuilder afterUpdateRuleFilter.queryBuilder afterAddRule.queryBuilder afterDeleteRule.queryBuilder afterUpdateRuleValue.queryBuilder afterUpdateRuleOperator.queryBuilder afterUpdateGroupCondition.queryBuilder', function () {
                                     var $this = mQuery(this),
-                                        rules = $this.queryBuilder('getRules', {get_flags: true}),
+                                        rules = $this.queryBuilder('getRules', {
+                                            get_flags: true,
+                                            skip_empty: true,
+                                            allow_invalid: true
+                                        }),
                                         raw = JSON.stringify(rules),
                                         $input = $this.parent().find('textarea');
-                                    if (rules && $input.length && $input.val() !== raw) {
-                                        $input.val(raw);
+                                    if ($input.length && $input.val() !== raw) {
+                                        $input.val(raw).trigger('change');
                                     }
                                 })
                                 .parent().find('textarea').addClass('hide');
