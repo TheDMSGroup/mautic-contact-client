f94f8b76cb1211749f55151553327b9e7e755f91
heathdutton <hdutton@thedmsgrp.com>
2019-03-06 09:21:47 -0500

    Iterate JS versions.

--- Views/ContactClient/datatable.html.php
+++ Views/ContactClient/datatable.html.php
@@ -20,6 +20,6 @@
     var tableData = <?php echo json_encode($tableData); ?>;
 </script>
 <?php
-    echo $view['assets']->includeScript('plugins/MauticContactClientBundle/Assets/build/contactclient.min.js', 'contactclientOnLoad', 'contactclientOnLoad');
+    echo $view['assets']->includeScript('plugins/MauticContactClientBundle/Assets/build/contactclient.min.js?v=2', 'contactclientOnLoad', 'contactclientOnLoad');
     echo $view['assets']->includeStylesheet('plugins/MauticContactClientBundle/Assets/build/contactclient.min.css');
 ?>
