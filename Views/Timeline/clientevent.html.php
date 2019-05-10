1dd1c2c2881f84e09c6988b79db03222f4e0e2ff
Dean Martin <contact@deanmartin.pro>
2019-05-10 15:09:36 -0400

    [ENG-873] Decided to just inline the javascript to make it easier.

--- Views/Timeline/clientevent.html.php
+++ Views/Timeline/clientevent.html.php
@@ -13,87 +13,6 @@ $message = $event['extra']['message'];
 $logs    = $event['extra']['logs'];
 ?>
 
-<style>
-
-/* Custom theme to be more friendly to non-developers */
-.cm-s-cc {
-    background: #fff;
-    color: #424242;
-    line-height: 22px;
-    font-family: 'Roboto Mono', Menlo, Monaco, Consolas, "Courier New", monospace !important;
-    font-size: 13px;
-    font-weight: normal;
-    padding: 0px;
-}
-
-.cm-s-cc .CodeMirror-gutters {
-    background: #fff;
-    color: #616161;
-    border: none;
-}
-/*.cm-s-cc .CodeMirror-cursors { visibility: visible !important; }*/
-.cm-s-cc .CodeMirror-cursor { border-left: 1px solid #303030 !important; background: #303030; width: 2px; }
-.cm-s-cc .CodeMirror-guttermarker,
-.cm-s-cc .CodeMirror-guttermarker-subtle,
-.cm-s-cc .CodeMirror-linenumber { color: rgb(83,127,126); }
-.cm-s-cc .CodeMirror-activeline-background { background: #b0bec5; }
-/*.cm-s-cc .CodeMirror-selected { background: #b0bec5; }*/
-.cm-s-cc .cm-comment { color: #616161; }
-.cm-s-cc .cm-string { color: #009688; }
-.cm-s-cc .cm-string-2 { color: #80CBC4; }
-.cm-s-cc .cm-number { color: #7e57c2; }
-.cm-s-cc .cm-atom { color: #7e57c2; }
-.cm-s-cc .cm-keyword { color: #ff5722; }
-.cm-s-cc .cm-variable { color: #009688; }
-.cm-s-cc .cm-def { color: #FD971F; }
-.cm-s-cc .cm-property { color: #0288d1; }
-.cm-s-cc .cm-tag { color: #1c75d1; }
-.cm-s-cc .cm-error { color: #EC5F67; }
-.cm-s-cc .cm-meta { color: #80CBC4; }
-.cm-s-cc .cm-operator { color: rgb(60, 73, 128); }
-.cm-s-cc .cm-variable-2 { color: #80CBC4; }
-.cm-s-cc .cm-variable-3 { color: #82B1FF; }
-.cm-s-cc .cm-builtin { color: #ad9e53; }
-.cm-s-cc .cm-attribute { color: #caa155; }
-.cm-s-cc .cm-qualifier { color: #a69850; }
-.cm-s-cc .CodeMirror-matchingbracket { font-weight: bold; color: #3c4980 !important; }
-
-/* Codemirror Mustache tags to style like our tag-editor tags (sorta) */
-.cm-mustache,
-.cm-mustache-danger,
-.cm-mustache-warn {
-    color: #424242;
-    background-color: rgba(187, 210, 236, 0.8);
-    border-radius: 3px;
-    padding: 2px;
-}
-
-.cm-mustache-danger {
-    /*background-color: #d9534f;*/
-    background-color: rgba(255, 124, 114, 0.8);
-}
-
-.cm-mustache-warn {
-    /*background-color: #e2a231;*/
-    background-color: rgba(255, 195, 86, 0.8);
-}
-.cm-mustache.CodeMirror-matchingbracket,
-.cm-mustache-danger.CodeMirror-matchingbracket,
-.cm-mustache-warn.CodeMirror-matchingbracket {
-    color: #424242 !important;
-    font-weight: bold;
-    border-radius: 3px;
-    padding: 2px 0;
-    /*margin: 0 -1px;*/
-}
-
-.cm-mustache.CodeMirror-selected,
-.cm-mustache-danger.CodeMirror-selected,
-.cm-mustache-warn.CodeMirror-selected {
-    background: #fff;
-}
-</style>
-
 <dl class="dl-horizontal small">
     <dt>Message:</dt>
     <dd><?=$message; ?></dd>
@@ -105,24 +24,22 @@ $logs    = $event['extra']['logs'];
 </dl>
 
 <script defer>
-$buttons = mQuery('.contact-client-button').parent().parent();
-$buttons.on('click', function(){ 
-    console.log('lol');
-    mQuery('textarea.codeMirror-json').each(function(i, element){ 
-        if(mQuery(element).is(':visible')) {
-        CodeMirror.fromTextArea(element, {
-            mode: {
-                name: 'javascript',
-                json: true
-            },
-            theme: 'cc',
-            gutters: [],
-            lineNumbers: false,
-            lineWrapping: true,
-            readOnly: true
-        });
-        }
-});
-
-}); 
+    $buttons = mQuery('.contact-client-button').parent().parent();
+    $buttons.on('click', function(){ 
+        mQuery('textarea.codeMirror-json').each(function(i, element){ 
+            if(mQuery(element).is(':visible')) {
+            CodeMirror.fromTextArea(element, {
+                mode: {
+                    name: 'javascript',
+                    json: true
+                },
+                theme: 'cc',
+                gutters: [],
+                lineNumbers: false,
+                lineWrapping: true,
+                readOnly: true
+            });
+            }
+    }); 
+    }); 
 </script>
